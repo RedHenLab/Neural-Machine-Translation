af9e0681c2ec4ef2fb857f480bbb1cbc00d96c03
Vikrant Goyal <vikrantgoyal97@gmail.com>
2018-05-25 15:35:10 +0530

    a minor change in make_data.sh

--- scripts/make_data.sh
+++ scripts/make_data.sh
@@ -5,14 +5,14 @@ lang=${1}-${2}
 export DATA_PREP=$DATA/$lang
 
 python ../preprocess.py \
-  -train_src $DATA_PREP/train.$lang.$src \
-  -train_tgt $DATA_PREP/train.$lang.$tgt \
-  -train_xe_src $DATA_PREP/train.$lang.$src \
-  -train_xe_tgt $DATA_PREP/train.$lang.$tgt \
-  -train_pg_src $DATA_PREP/train.$lang.$src \
-  -train_pg_tgt $DATA_PREP/train.$lang.$tgt \
-  -valid_src $DATA_PREP/valid.$lang.$src \
-  -valid_tgt $DATA_PREP/valid.$lang.$tgt \
-  -test_src $DATA_PREP/test.$lang.$src \
-  -test_tgt $DATA_PREP/test.$lang.$tgt \
+  -train_src $DATA_PREP/train.$lang.$src.processed \
+  -train_tgt $DATA_PREP/train.$lang.$tgt.processed \
+  -train_xe_src $DATA_PREP/train.$lang.$src.processed \
+  -train_xe_tgt $DATA_PREP/train.$lang.$tgt.processed \
+  -train_pg_src $DATA_PREP/train.$lang.$src.processed \
+  -train_pg_tgt $DATA_PREP/train.$lang.$tgt.processed \
+  -valid_src $DATA_PREP/valid.$lang.$src.processed \
+  -valid_tgt $DATA_PREP/valid.$lang.$tgt.processed \
+  -test_src $DATA_PREP/test.$lang.$src.processed \
+  -test_tgt $DATA_PREP/test.$lang.$tgt.processed \
   -save_data $DATA_PREP/processed_all
